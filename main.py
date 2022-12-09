754c22fbd71c7c8dd141a97640f9cb31ca96ea4b
mvam <marcos12144@hotmail.com>
2022-12-08 21:29:48 -0300

    adicionando playback

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sys import exit
 from ctypes import windll
 from pygame import init, quit, display, time
 import game
+import record
 import players.a_star
 import players.monte_carlo
 
@@ -11,15 +12,55 @@ DOWN = 2
 LEFT = 3
 
 
-def gameOver(snake: tuple[list[int]], border: tuple[list[int]], display_range: int, time_game_fps: int, score: int, time_in_game: str) -> None:
+def playback(playback_number: int, time_game_fps: int):
+    init()
+    display_range, snake, moves = record.recordReadStart(playback_number)
+    snake_skin, apple, border = game.subConstructs(display_range)
+
+    screen = display.set_mode((display_range, display_range))
+    display.set_caption(f'Playback {playback_number}')
+    
+    clock = time.Clock()
+    start_time = time.get_ticks()
+    score = 0
+    time_in_game = 0
+    snake_direction = None
+    apple_pos = None
+    for m in moves:
+        if m.__contains__('DEATH'):
+            break
+
+        snake_direction, apple_pos_file = record.recordReadMoveFormat(m)
+        if not apple_pos:
+            apple_pos = apple_pos_file
+            newApple = False
+        else:
+            newApple = apple_pos != apple_pos_file
+
+        snake, apple_pos, score = game.snakeMoviment(
+            tuple(snake), snake_direction, apple_pos, display_range, score)
+
+        if newApple:
+            apple_pos = apple_pos_file
+
+        # region Screen/Display
+        time_in_game = game.display_screen(screen, score, clock, time_game_fps, snake, snake_skin, apple, apple_pos, start_time)
+        display.update()
+        # endregion
+
+    record.finishPlayback(snake, display_range, border, score, time_in_game)
+
+
+def gameOver(snake: tuple[list[int]], border: tuple[list[int]], display_range: int, time_game_fps: int, score: int, time_in_game: str, record_str: str) -> None:
     """ Return the game over screen an quit game if some rule of lose is cacthed """
     decision = 0
     MessageBox = windll.user32.MessageBoxW
     if game.win(snake, display_range):
+        record.finishRecord(record_str, time_in_game)
         decision = MessageBox(
             None, f'You Win!!! Score: {score}.', f'Game Over in {time_in_game} minutes.', 5)
     elif game.lose(list(snake)[0], tuple(snake), border):
-        # game.record_move_file(record)
+        record.finishRecord(record_str, time_in_game)
         decision = MessageBox(
             None, f'You lose. Score: {score}', f'Game Over at {time_in_game} minutes.', 5)
     if decision == 2:
@@ -42,7 +83,7 @@ def program(name: str, display_range: int, time_game_fps: int):
     score = 0
     astar = players.a_star.AStar()
     # monteCarlo = players.monte_carlo.MonteCarlo()
-    record = []
+    record_str = record.startRecord(display_range, snake)
     while True:
         # region Input
         eventAI = astar.getKey(apple_pos, tuple(snake),
@@ -55,28 +96,22 @@ def program(name: str, display_range: int, time_game_fps: int):
         snake, apple_pos, score = game.snakeMoviment(
             tuple(snake), snake_direction, apple_pos, display_range, score)
 
-        record.append((snake_direction, apple_pos))
-
         # region Screen/Display
-        game.display_score(screen, score)
-        clock.tick(time_game_fps)  # refresh rate
-
-        screen.fill((0, 0, 0))
-        screen.blit(apple, apple_pos)
-        for pos in snake:
-            screen.blit(snake_skin, pos)
-        time_in_game = game.display_time(screen, start_time)
-        game.display_score(screen, score)
+        time_in_game = game.display_screen(screen, score, clock, time_game_fps, snake, snake_skin, apple, apple_pos, start_time)
         display.update()
         # endregion
         
+        record_str += record.recordMoveFormat(snake_direction, apple_pos)
         gameOver(snake, border, display_range,
-                 time_game_fps, score, time_in_game)
+                 time_game_fps, score, time_in_game, record_str)
 
 
 if __name__ == '__main__':
     name = 'Snake AI'
-    time_game = 200
-    display_range = 200
+    time_game = 100
+    display_range = 100
 
     program(name, display_range, time_game)
+
+    record_number = 0
+    playback(record_number, time_game)
